6ded72ce19d93a0c47687f510d4c0154d9691467
Nathan6141 <naal2536@colorado.edu>
2024-04-17 13:46:24 -0600

    Update main.js

--- ica13/main.js
+++ ica13/main.js
@@ -1,13 +1,8 @@
-// setup canvas
-
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
-
-
-
 
 // function to generate random number
 
@@ -22,95 +17,87 @@ function randomRGB() {
 }
 
 class Ball {
-  constructor(x, y, velX, velY, color, size) {
-    this.x = x;
-    this.y = y;
-    this.velX = velX;
-    this.velY = velY;
-    this.color = color;
-    this.size = size;
-  }
-  draw() {
-    ctx.beginPath();
-    ctx.fillStyle = this.color;
-    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-    ctx.fill();
-  }
-  update() {
-    if ((this.x + this.size) >= width) {
-      this.velX = -(this.velX);
+    constructor(x, y, velX, velY, color, size) {
+        this.x = x;
+        this.y = y;
+        this.velX = velX;
+        this.velY = velY;
+        this.color = color;
+        this.size = size;
     }
-  
-    if ((this.x - this.size) <= 0) {
-      this.velX = -(this.velX);
+
+    draw() {
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+        ctx.fill();
     }
-  
-    if ((this.y + this.size) >= height) {
-      this.velY = -(this.velY);
-    }
-  
-    if ((this.y - this.size) <= 0) {
-      this.velY = -(this.velY);
-    }
-  
-    this.x += this.velX;
-    this.y += this.velY;
-  }
-  collisionDetect() {
-    for (const ball of balls) {
-      if (this !== ball) {
-        const dx = this.x - ball.x;
-        const dy = this.y - ball.y;
-        const distance = Math.sqrt(dx * dx + dy * dy);
-  
-        if (distance < this.size + ball.size) {
-          ball.color = this.color = randomRGB();
+
+    update() {
+        if ((this.x + this.size) >= width) {
+          this.velX = -(this.velX);
         }
+      
+        if ((this.x - this.size) <= 0) {
+          this.velX = -(this.velX);
+        }
+      
+        if ((this.y + this.size) >= height) {
+          this.velY = -(this.velY);
+        }
+      
+        if ((this.y - this.size) <= 0) {
+          this.velY = -(this.velY);
+        }
+      
+        this.x += this.velX;
+        this.y += this.velY;
       }
+    
+      collisionDetect() {
+        for (const ball of balls) {
+            if (this !== ball) {
+                const dx = ball.x - this.x;
+                const dy = ball.y - this.y;
+                const distance = Math.sqrt(dx * dx + dy * dy);
+    
+                if (distance < this.size + ball.size) {
+                    // Reverse velocities along collision line
+                    [this.velX, ball.velX] = [ball.velX, this.velX];
+                    [this.velY, ball.velY] = [ball.velY, this.velY];
+                }
+            }
+        }
     }
-  }
-}
-
-
-
-// const testBall = new Ball(50, 100, 4, 4, "blue", 10);
-
-//testBall.x;
-//testBall.size;
-//testBall.color;
-//testBall.draw();
+}   
 
 const balls = [];
 
 while (balls.length < 25) {
-  const size = random(10, 20);
-  const ball = new Ball(
-    // ball position always drawn at least one ball width
-    // away from the edge of the canvas, to avoid drawing errors
-    random(0 + size, width - size),
-    random(0 + size, height - size),
-    random(-7, 7),
-    random(-7, 7),
-    randomRGB(),
-    size,
-  );
+    const size = random(1,20)
+    const ball = new Ball(
+        random(0 + size, width - size),
+        random(0 + size, height - size),
+        random(-10, 10),
+        random(-10, 10),
+        randomRGB(),
+        size,
+    )
 
-  balls.push(ball);
-}
+    balls.push(ball);
+    }
 
-function loop() {
-  ctx.fillStyle = "rgb(0 0 0 / 25%)";
-  ctx.fillRect(0, 0, width, height);
+function loop () {
+    ctx.fillStyle = "rgb( 0 0 0 / 25%)";
+    ctx.fillRect(0, 0, width, height);
 
-  for (const ball of balls) {
-    ball.draw();
-    ball.update();
-    ball.collisionDetect();
-  }
-
-  requestAnimationFrame(loop);
+    for (const ball of balls) {
+        ball.draw();
+        ball.update();
+        ball.collisionDetect();
+    }
+    requestAnimationFrame(loop);
 }
 
 loop();
-
 
