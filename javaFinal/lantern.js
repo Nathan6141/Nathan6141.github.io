cfb26c88502a4a3868189b5f83d2f13628e3cd29
Nathan6141 <157062868+Nathan6141@users.noreply.github.com>
2026-02-17 00:02:16 -0700

    Update lantern.js

--- javaFinal/lantern.js
+++ javaFinal/lantern.js
@@ -1,8 +1,2 @@
-const playShow = document.getElementById('playShow');
-
-// Add click event
-playShow.addEventListener('click', function() {
-    alert('button pressed');
-});
 
 // if pressed 'Play Show' then play show else play idle rgb show
